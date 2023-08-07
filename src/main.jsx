bc9b5fa58c6b1595af38af03200f805a71b19077
jefti <jeftimeira@gmail.com>
2023-08-07 20:16:51 -0300

    feature: Reset CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import Reset from './styles/Reset.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Reset/>
     <App />
   </React.StrictMode>,
 )
