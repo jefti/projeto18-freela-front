dc7f2bc5611bba98acf4245e26352aab1eec4b39
jefti <jeftimeira@gmail.com>
2023-08-07 20:20:26 -0300

    feature: Style.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
 import Reset from './styles/Reset.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
