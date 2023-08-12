6b21be91b25433730032ff1934f0e49a3f2d56e5
jefti <jeftimeira@gmail.com>
2023-08-12 12:59:29 -0300

    feature: Acess control

--- src/pages/managePokemonPage.jsx
+++ src/pages/managePokemonPage.jsx
@@ -1,11 +1,20 @@
+import { useContext, useEffect } from "react";
 import ScreenWithBars from "../components/screenWithBars/ScreenWithBars.jsx";
 import { DefaultPageContainer } from "../styles/formStyle";
+import { UserContext } from "../contexts/userContext.jsx";
+import { useNavigate } from "react-router-dom";
 
 export default function ManagePokemonPage(){
+    const {user} = useContext(UserContext);
+    const nav = useNavigate();
+    useEffect(()=>{
+        if(!user.token) nav('/');
+    },[])
+
     return (
         <ScreenWithBars>
             <DefaultPageContainer>
-                Manage Pokemon
+            {user.token?"nome :" + user.nome: "Não está logado"}
             </DefaultPageContainer>
         </ScreenWithBars>
     )
--- src/pages/registerPokemon.jsx
+++ src/pages/registerPokemon.jsx
@@ -1,11 +1,21 @@
+import { useContext, useDebugValue, useEffect } from "react";
 import ScreenWithBars from "../components/screenWithBars/ScreenWithBars.jsx";
 import { DefaultPageContainer } from "../styles/formStyle";
+import { UserContext } from "../contexts/userContext.jsx";
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterPokemonPage(){
+    const {user} = useContext(UserContext);
+    const nav = useNavigate();
+    
+    useEffect(()=>{
+        if(!user.token) nav('/');
+    },[]);
+
     return (
         <ScreenWithBars>
             <DefaultPageContainer>
-                Register Pokemon
+                {user.token?"nome :" + user.nome: "Não está logado"}
             </DefaultPageContainer>
         </ScreenWithBars>
     )
